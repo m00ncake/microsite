87536bd15fb3d4fa39042ef198ec9403fc27727e
John Moon <john@John-Moon.local>
2018-10-11 08:36:36 -0700

    new_pricing_page.html & scss added

--- js/microsite.js
+++ js/microsite.js
@@ -1,5 +1,7 @@
 (function ($) {  
-    // Home Page
+    /***************************/
+    //        Home Page        //
+    /***************************/
     // Microsite NavBar Mod
     // $(".microsite a.navbar-brand").attr('href', '/sms');
     // $(".microsite .menu-393 a, .microsite .menu-1832 a, .microsite .menu-3317 a, .microsite .menu-1566 a").css('opacity', '1');
@@ -87,7 +89,9 @@
     });
 
     
-    // Features Page
+    /****************************/
+    //      Features Page       //
+    /****************************/
     $('.section').on('click', '.feat-right--bottom-content h2', function() {
         console.log("link clicked");
         console.log(this);
@@ -108,9 +112,10 @@
         $(this).siblings('.video').attr('src','');
         $(this).siblings('.video').attr('src',video);
     });
-    /*****************/
-    // Pricings Page //
-    /*****************/
+
+    /****************************/
+    //      Pricings Page       //
+    /****************************/
     // Pricing Page Right Arrow link mod
     function pricingLinkChange() {
         $(".microsite .pricing4 .boxes a.price-arrow.price-arrow-right.fa.fa-arrow-right").attr('href', '/sms/high-volume-pricing');
@@ -156,9 +161,169 @@
     });
 
 
-    /******************/
-    //  Sign Up Page  //
-    /******************/
+    /***********************************/
+    //     Pricing Selector Page       //
+    /***********************************/
+    var selectedPlan = 'monthly';
+
+    function setPrice(price, message, keyword) {
+        $('.slider-suggestion h3 span').text(price);
+        $('.pkg-desc span.text-amount').text(message);
+    } 
+
+    function setPIDs(pid) {
+        $('#sign-up-btn').attr('href','/start?pid=' + pid );
+    }
+    
+    $('.monthly-plan').click(function(){
+        selectedPlan = 'monthly';
+        $('.alert-annual').addClass('not-visible');
+        $('.text-amount').text('1,000');
+        $('.slider-list li.active').trigger('click');
+    });  
+    $('.annual-plan').click(function(){
+        selectedPlan = 'annually';
+        $('.alert-annual').removeClass('not-visible');
+        $('.text-amount').text('12,000');
+        $('.slider-list li.active').trigger('click');
+    }); 
+
+    $('#pa').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$49', '500');
+            setPIDs('plus');
+        } else {
+            setPrice('$37', '6,000');
+            setPIDs('plusvolume');
+        }
+    });
+    $('#pb').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$94', '1,000');
+            setPIDs('select');
+        } else {
+            setPrice('$71', '12,000');
+            setPIDs('selectvolume');
+        }
+    });
+    $('#pc').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$149', '3,000');
+            setPIDs('elite');
+        } else {
+            setPrice('$112', '36,000');
+            setPIDs('elitevolume');
+        }
+    });
+    $('#pd').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$129');
+            setPIDs('pro');
+        } else {
+            setPrice('$97');
+            setPIDs('provolume');
+        }
+    });
+    $('#pe').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$250', '5,000');
+            setPIDs('pro');
+        } else {
+            setPrice('$188', '60,000');
+            setPIDs('provolume');
+        }
+    });
+    $('#pf').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$450', '10,000');
+            setPIDs('bronze');
+        } else {
+            setPrice('$338', '120,000');
+            setPIDs('bronzevolume');
+        }
+    });
+    $('#pg').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$1100', '20,000');
+            setPIDs('silver');
+        } else {
+            setPrice('$825', '240,000');
+            setPIDs('silvervolume');
+        }
+    });
+    $('#ph').click(function() {
+        if(selectedPlan === 'monthly') {
+            setPrice('$2000', '50,000');
+            setPIDs('gold');
+        } else {
+            setPrice('$1500', '600,000');
+            setPIDs('goldvolume');
+        }
+    });
+
+    $('.slider-list li').click(function(){
+        $('.slider-list li').removeClass('active');
+        $(this).addClass('active');
+        $('.slider-suggestion, .slider-actions').removeClass('hide');
+        $('#more').addClass('hide');
+    });
+    $('#pi').click(function(){
+        $('.slider-suggestion, .slider-actions').addClass('hide');
+        $('#more').removeClass('hide');
+    });
+  
+    /*********** Original CodePen Code from Skinner *************/
+
+    $('.annual-plan').click(function(){
+        $('.alert-annual').removeClass('not-visible');
+        $('.text-amount').text('12,000');
+      });
+      $('.monthly-plan').click(function(){
+        $('.alert-annual').addClass('not-visible');
+        $('.text-amount').text('1,000');
+      });
+      $('.slider-list li').click(function(){
+        $('.slider-list li').removeClass('active');
+        $(this).addClass('active');
+        $('.slider-suggestion, .slider-actions').removeClass('hide');
+        $('#more').addClass('hide');
+      });
+      $('#pa').click(function(){
+        $('.slider-suggestion h3 span').text('$49');
+      });
+      $('#pb').click(function(){
+        $('.slider-suggestion h3 span').text('$94');
+      });
+      $('#pc').click(function(){
+        $('.slider-suggestion h3 span').text('$149');
+      });
+      $('#pd').click(function(){
+        $('.slider-suggestion h3 span').text('$129');
+      });
+      $('#pe').click(function(){
+        $('.slider-suggestion h3 span').text('$250');
+      });
+      $('#pf').click(function(){
+        $('.slider-suggestion h3 span').text('$450');
+      });
+      $('#pg').click(function(){
+        $('.slider-suggestion h3 span').text('$1100');
+      });
+      $('#ph').click(function(){
+        $('.slider-suggestion h3 span').text('$2000');
+      });
+      $('#pi').click(function(){
+        $('.slider-suggestion, .slider-actions').addClass('hide');
+        $('#more').removeClass('hide');
+      });
+  
+
+
+
+
+    /***********************************/
+    //          Sign Up Page           //
+    /***********************************/
     // the two links under the submit button
     // $(".microsite .text-muted a:nth-child(1)").attr('href', '/terms-service-0');
     // $(".microsite .text-muted a:nth-child(2)").attr('href', '/anti-spam-policy-0');
@@ -205,36 +370,64 @@
     //              VWO Cookies              //
     /*****************************************/
     function checkCookie() {
-        var $vwoCookie = Cookies.get('_vis_opt_exp_376_combi');
-        //  VWO Control Group  //
-        if ($vwoCookie == '1') {
-            console.log("We're in Control");
+
+        var $vwoFreeTrialCookie = Cookies.get('_vis_opt_exp_376_combi');
+        var $vwoVolumeCookie = Cookies.get('_vis_opt_exp_378_combi');
+        //  only Free Trial Cookie without Volume Cookie  //
+        if ($vwoFreeTrialCookie == '1') {
+            console.log("Volume: none, FreeTrial: 1");
             //   Home Page CTA href mods  //
             $('a.btn.btn-rounded.btn-orange.btn-sign-up').attr('href','/start?pid=free');
             $('a.header-btns--try').attr('href','/start?pid=free');
             $('.form-sect8-btn a').attr('href','/start?pid=free');
-            //   Pricing Page Pricing Block Toggle  //
-            $('#freeEZ').attr('style','display: block');    
-            $('#freetrial').attr('style','display: none');
-        //  VWO Variation 1  //
-        } else if ($vwoCookie == '2'){  
-            console.log("We're in Variation 1");
+        } else if ($vwoFreeTrialCookie == '2') {
+            console.log("Volume: none, FreeTrial: 2");
             //   Home Page CTA href mods  //
             $('a.btn.btn-rounded.btn-orange.btn-sign-up').attr('href','/start?pid=freetrial');
             $('a.header-btns--try').attr('href','/start?pid=freetrial');
             $('.form-sect8-btn a').attr('href','/start?pid=freetrial');
             //   Pricing Page Pricing Block Toggle  //
-            $('#freeEZ').attr('style','display: none');    
-            $('#freetrial').attr('style','display: block');  
+            $('#freeEZ').attr('style','display: none');
+            $('#freetrial').attr('style','display: block');
         }
-        var $vwoCookie = Cookies.get('_vis_opt_exp_378_combi');
+        
         //  VWO Control Group  //
-        if ($vwoCookie == '1') {
+        if ($vwoVolumeCookie == '1') {
         //  VWO Variation 1  //
-        } else if ($vwoCookie == '2'){  
+            if ($vwoFreeTrialCookie == '1') {
+                console.log("Volume: 1, FreeTrial: 1");
+                //   Home Page CTA href mods  //
+                $('a.btn.btn-rounded.btn-orange.btn-sign-up').attr('href','/start?pid=free');
+                $('a.header-btns--try').attr('href','/start?pid=free');
+                $('.form-sect8-btn a').attr('href','/start?pid=free');
+            } else if ($vwoFreeTrialCookie == '2') {
+                console.log("Volume: 1, FreeTrial: 2");
+                //   Home Page CTA href mods  //
+                $('a.btn.btn-rounded.btn-orange.btn-sign-up').attr('href','/start?pid=freetrial');
+                $('a.header-btns--try').attr('href','/start?pid=freetrial');
+                $('.form-sect8-btn a').attr('href','/start?pid=freetrial');
+                //   Pricing Page Pricing Block Toggle  //
+                $('#freeEZ').attr('style','display: none');
+                $('#freetrial').attr('style','display: block');
+            }
+        } else if ($vwoVolumeCookie == '2'){  
             //   Home Page CTA href mods  //
             $('body').addClass('free-volume-pricing');
             $('a[href="/start"').attr('href', '/start?pid=paygovolume');
+            if ($vwoFreeTrialCookie == '1') {
+                console.log("Volume: 2, FreeTrial: 1");
+                $('#freeEZ').attr('style','display: none');
+                $('#freetrial').attr('style','display: none');
+                $('#volumefreeEZ').attr('style','display: block');
+                $('#volumefreetrial').attr('style','display: none');
+                
+            } else if ($vwoFreeTrialCookie == '2') {
+                console.log("Volume: 2, FreeTrial: 2");
+                $('#freeEZ').attr('style','display: none');
+                $('#freetrial').attr('style','display: none');
+                $('#volumefreeEZ').attr('style','display: none');
+                $('#volumefreetrial').attr('style','display: block');
+            }
         }
     }
     checkCookie();
